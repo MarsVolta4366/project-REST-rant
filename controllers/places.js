85290108e9c6bff1b4fbca1e15e8b73a34b899c6
unknown <derek.slauson5@gmail.com>
2021-12-07 12:35:46 -0600

    Form data is getting through

--- controllers/places.js
+++ controllers/places.js
@@ -1,4 +1,12 @@
+// On part six
 const router = require('express').Router()
+var bodyParser = require('body-parser')
+
+// create application/json parser
+//var jsonParser = bodyParser.json()
+
+// create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 router.get("/new", (req, res) => {
     res.render("places/New")
@@ -11,18 +19,20 @@ router.get("/", (req, res) => {
         state: 'WA',
         cuisines: 'Thai, Pan-Asian',
         pic: '/images/thaiImage.jpg'
-        }, {
+    }, {
         name: 'Coding Cat Cafe',
         city: 'Phoenix',
         state: 'AZ',
         cuisines: 'Coffee, Bakery',
         pic: 'images/catCafeImage.jpg'
-        }]
-    res.render("places/index", {places})
+    }]
+    res.render("places/index", { places })
 })
 
-router.post("/", (req, res) => {
-    console.log(req)
+router.post("/", urlencodedParser, (req, res) => {
+    const obj = JSON.parse(JSON.stringify(req.body)); // req.body = [Object: null prototype] { placeName: 'test', pic: '', city: 'test', state: 'test', cuisines: 'test' }
+
+    console.log(obj); // { placeName: 'test', pic: '', city: 'test', state: 'test', cuisines: 'test' }
     res.send("POST /places")
 })
 
