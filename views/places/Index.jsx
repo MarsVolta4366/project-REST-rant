7bc1aa10b964d97e95825c013e8361ddd06d20be
unknown <derek.slauson5@gmail.com>
2021-12-08 19:21:17 -0600

    Navbar with a little style

--- views/places/Index.jsx
+++ views/places/Index.jsx
@@ -2,9 +2,9 @@ const React = require("react")
 const Def = require("../Default")
 
 function Index(data) {
-    let placesFormatted = data.places.map((place) => {
+    let placesFormatted = data.places.map((place, index) => {
         return (
-            <div className="col-sm-6">
+            <div className="col-sm-6" key={index}>
                 <h2>{place.name}</h2>
                 <p className="text-center">{place.cuisines}</p>
                 <img src={place.pic} alt={place.name}/>
