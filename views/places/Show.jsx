2fc296241d17196f00868571003b8b3c441fdca4
unknown <derek.slauson5@gmail.com>
2021-12-16 08:59:01 -0600

    Added showEstablished() to show page

--- views/places/Show.jsx
+++ views/places/Show.jsx
@@ -6,16 +6,18 @@ function Show(data) {
         <Def>
             <main>
                 <h1>{data.place.name}</h1>
-                <p>{data.place.cuisines}</p>
+                <p>Serving {data.place.cuisines}</p>
                 <img src={data.place.pic} alt={data.place.name} />
                 <p>Located in {data.place.city}, {data.place.state}</p>
-                <h3>{data.place.showEstablished()}</h3>
+                <h2>Description</h2>
+                <p>{data.place.showEstablished()}</p>
                 <a href={`/places/${data.id}/edit`} className="btn btn-warning">Edit</a>
 
                 <form method="POST" action={`/places/${data.id}?_method=DELETE`}>
                     <button type="submit" className="btn btn-danger">Delete</button>
                 </form>
                 <div>
+                    <h2>Rating</h2>
                     <p>Currently unrated</p>
                 </div>
                 <div>
